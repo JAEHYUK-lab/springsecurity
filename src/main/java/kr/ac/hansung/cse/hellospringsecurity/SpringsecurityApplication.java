54623a5880b9a4054da499f1686f8b6da6dd57bd
JAEHYUK-lab <afentis508@gmail.com>
2021-07-19 14:48:52 +0900

    springsecurity

---
+++ src/main/java/kr/ac/hansung/cse/hellospringsecurity/SpringsecurityApplication.java
@@ -0,0 +1,14 @@
+package kr.ac.hansung.cse.hellospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityApplication.class, args);
+    }
+
+}
